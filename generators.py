4fcf05f20166aab4d9455d41d8a9fa48fb960149
il12 <il5498@yandex.ru>
2018-05-23 11:44:56 +0300

    Done!

---
+++ generators.py
@@ -0,0 +1,26 @@
+class Fibonacci:
+    def __init__(self,n):
+        self.lst_fib = (lambda n, fib=[0,1]: fib[:n]+[fib.append(fib[-1] + fib[-2]) or fib[-1] for i in range(n-len(fib))])(n)
+
+    def generator(self,n):
+        fib1, fib2 = 0, 1
+        yield fib1
+        for i in range(n):
+            fib1, fib2 = fib2, fib1 + fib2
+            yield fib1
+
+    def next(self):
+        self.generator.next()
+
+F = Fibonacci()
+x = F.generator(10)
+print(F.lst_fib)
+print(list(F.generator(100)))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
